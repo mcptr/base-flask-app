53f3a9d67136f3528b496b1c56e764678106df97
Bartłomiej Kurek <dev@metaceptron.com>
2019-06-17 11:56:46 +0200

    Added logging function. Updated .gitignore.

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import flask
+import json
 
 
 app = flask.Flask(__name__)
+
+
+def log_request(req_dict):
+    """Stores request information in db"""
+    with open("log-requests.txt", "a+") as fh:
+        fh.write(json.dumps(req_dict))
 
 
 if __name__ == "__main__":
