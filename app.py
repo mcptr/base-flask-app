a58e6a5c04c4cde2bd1a323c1c31208b5f5c534f
Bartłomiej Kurek <dev@metaceptron.com>
2019-06-17 12:03:13 +0200

    Added after_request flask hook.

--- app.py
+++ app.py
@@ -7,8 +7,18 @@ app = flask.Flask(__name__)
 
 def log_request(req_dict):
     """Stores request information in db"""
-    with open("log-requests.txt", "a+") as fh:
-        fh.write(json.dumps(req_dict))
+    with open("tmp/requests.log", "a+") as fh:
+        fh.write(json.dumps(req_dict) + "\n")
+
+
+@app.after_request
+def log_request_callbacks(response):
+    log_request(dict(
+        remote_addr=flask.request.remote_addr,
+        path=flask.request.path,
+    ))
+
+    return response
 
 
 if __name__ == "__main__":
